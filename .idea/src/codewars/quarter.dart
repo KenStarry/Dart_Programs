59c99cc5a0509d647e7ce1b8ac3472c4fe97303b
KenStarry <starrycodes@gmail.com>
2023-07-11 16:51:30 +0300

    Quarter

---
+++ .idea/src/codewars/quarter.dart
@@ -0,0 +1,11 @@
+int quarter(int month) {
+  if (month <= 3) {
+    return 1;
+  } else if (month <= 6 && month > 3) {
+    return 2;
+  } else if (month <= 9 && month > 6) {
+    return 3;
+  } else {
+    return 4;
+  }
+}
