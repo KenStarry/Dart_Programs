aabb7991e19ecdfe8d5c808af001d72f8d694c2c
KenStarry <starrycodes@gmail.com>
2023-07-19 08:51:07 +0300

    Beginner dart series alternative

--- .idea/src/codewars/beginner_series.dart
+++ .idea/src/codewars/beginner_series.dart
@@ -1,1 +1,2 @@
 int past(int h, int m, int s) => ((h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000));
+int pastAlt(int h, int m, int s) => Duration(hours: h, minutes: m, seconds: s).inMilliseconds;
